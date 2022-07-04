697ced7c142f231ea83e3d02dd9be8979f6c4674
2garam <2garamssi@gmail.com>
2022-07-04 20:51:39 +0900

    02. extract_function (메소드 추출)

--- src/main/java/me/whiteship/refactoring/_02_duplicated_code/_04_extract_function/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_02_duplicated_code/_04_extract_function/StudyDashboard.java
@@ -12,31 +12,43 @@ public class StudyDashboard {
 
     private void printParticipants(int eventId) throws IOException {
         // Get github issue to check homework
-        GitHub gitHub = GitHub.connect();
-        GHRepository repository = gitHub.getRepository("whiteship/live-study");
-        GHIssue issue = repository.getIssue(eventId);
+        GHIssue issue = getGhIssue(eventId);
 
         // Get participants
-        Set<String> participants = new HashSet<>();
-        issue.getComments().forEach(c -> participants.add(c.getUserName()));
+        Set<String> participants = getUserNames(issue);
 
         // Print participants
-        participants.forEach(System.out::println);
+        print(participants);
     }
 
     private void printReviewers() throws IOException {
         // Get github issue to check reviews
-        GitHub gitHub = GitHub.connect();
-        GHRepository repository = gitHub.getRepository("whiteship/live-study");
-        GHIssue issue = repository.getIssue(30);
+        GHIssue issue = getGhIssue(30);
 
         // Get reviewers
-        Set<String> reviewers = new HashSet<>();
-        issue.getComments().forEach(c -> reviewers.add(c.getUserName()));
+        Set<String> reviewers = getUserNames(issue);
 
         // Print reviewers
-        reviewers.forEach(System.out::println);
+        print(reviewers);
     }
+
+    private void print(Set<String> participants) {
+        participants.forEach(System.out::println);
+    }
+
+    private Set<String> getUserNames(GHIssue issue) throws IOException {
+        Set<String> participants = new HashSet<>();
+        issue.getComments().forEach(c -> participants.add(c.getUserName()));
+        return participants;
+    }
+
+    private GHIssue getGhIssue(int eventId) throws IOException {
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        GHIssue issue = repository.getIssue(eventId);
+        return issue;
+    }
+
 
     public static void main(String[] args) throws IOException {
         StudyDashboard studyDashboard = new StudyDashboard();

2f42befcb21e0a621127f38713c0a4a68122f62b
2garam <2garamssi@gmail.com>
2022-07-04 21:23:40 +0900

    원복

--- src/main/java/me/whiteship/refactoring/_03_long_function/_01_before/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_03_long_function/_01_before/StudyDashboard.java
@@ -72,22 +72,15 @@ public class StudyDashboard {
             writer.print(header(totalNumberOfEvents, participants.size()));
 
             participants.forEach(p -> {
-                String markdownForHomework = getMarkdownForParticipant(totalNumberOfEvents, p);
+                long count = p.homework().values().stream()
+                        .filter(v -> v == true)
+                        .count();
+                double rate = count * 100 / totalNumberOfEvents;
+
+                String markdownForHomework = String.format("| %s %s | %.2f%% |\n", p.username(), checkMark(p, totalNumberOfEvents), rate);
                 writer.print(markdownForHomework);
             });
         }
-    }
-
-    private double getRate(int totalNumberOfEvents, Participant p) {
-        long count = p.homework().values().stream()
-                .filter(v -> v == true)
-                .count();
-        double rate = count * 100 / totalNumberOfEvents;
-        return rate;
-    }
-
-    private String getMarkdownForParticipant(int totalNumberOfEvents, Participant p) {
-        return String.format("| %s %s | %.2f%% |\n", p.username(), checkMark(p, totalNumberOfEvents), getRate(totalNumberOfEvents, p));
     }
 
     /**

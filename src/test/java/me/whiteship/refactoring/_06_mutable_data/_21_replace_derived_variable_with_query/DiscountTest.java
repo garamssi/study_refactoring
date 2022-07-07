a059d00fb07d7318048b16f87e40e53eb4d8c19a
2garam <2garamssi@gmail.com>
2022-07-07 21:23:09 +0900

    06-21 mutable_data - replace derived variable 파생 변수를 질의 함수로 바꾸기
    : 값의 의해 생성된 변수를 final이 아닌 변수에 담아서 사용하면 에러를 발생 할 수 있기때문에 질의 함수로 변경하여 사용하기.

--- src/test/java/me/whiteship/refactoring/_06_mutable_data/_21_replace_derived_variable_with_query/DiscountTest.java
+++ src/test/java/me/whiteship/refactoring/_06_mutable_data/_21_replace_derived_variable_with_query/DiscountTest.java
@@ -9,8 +9,8 @@ class DiscountTest {
     @Test
     void discount() {
         Discount discount = new Discount(100);
-//        assertEquals(100, discount.getDiscountedTotal());
-
+        assertEquals(100, discount.getDiscountedTotal());
+        
         discount.setDiscount(10);
         assertEquals(90, discount.getDiscountedTotal());
     }

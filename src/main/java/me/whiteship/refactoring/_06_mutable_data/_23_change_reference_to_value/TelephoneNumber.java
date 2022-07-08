0dbb441d361ff8d7a2ab42d21e6fad6e4ad3e966
2garam <2garamssi@gmail.com>
2022-07-08 16:09:06 +0900

    06-23 mutable_data : change_reference_to_value 참조를 값으로 변경하기
    : 참조 값으로 리턴 하는 것이 아닌 값으로 ( 새로운 객체 )로 리턴하는 식으로 변경

--- src/main/java/me/whiteship/refactoring/_06_mutable_data/_23_change_reference_to_value/TelephoneNumber.java
+++ src/main/java/me/whiteship/refactoring/_06_mutable_data/_23_change_reference_to_value/TelephoneNumber.java
@@ -1,24 +1,53 @@
 package me.whiteship.refactoring._06_mutable_data._23_change_reference_to_value;
 
-public class TelephoneNumber {
+import java.util.Objects;
 
-    private String areaCode;
+//public class TelephoneNumber {
+//
+//    private final String areaCode;
+//
+//    private final String number;
+//
+//    public TelephoneNumber(String areaCode, String number) {
+//        this.areaCode = areaCode;
+//        this.number = number;
+//    }
+//
+//    public String areaCode() {
+//        return areaCode;
+//    }
+//
+//    public String number() {
+//        return number;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        TelephoneNumber number1 = (TelephoneNumber) o;
+//        return Objects.equals(areaCode, number1.areaCode) && Objects.equals(number, number1.number);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(areaCode, number);
+//    }
+//}
 
-    private String number;
+// 14버전 부터 사용할 수 있는 레코드로 사용하면 간편하게 사용할 수 있다.
+public record TelephoneNumber(String areaCode, String number) {
 
-    public String areaCode() {
-        return areaCode;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TelephoneNumber number1 = (TelephoneNumber) o;
+        return Objects.equals(areaCode, number1.areaCode) && Objects.equals(number, number1.number);
     }
 
-    public void areaCode(String areaCode) {
-        this.areaCode = areaCode;
-    }
-
-    public String number() {
-        return number;
-    }
-
-    public void number(String number) {
-        this.number = number;
+    @Override
+    public int hashCode() {
+        return Objects.hash(areaCode, number);
     }
 }

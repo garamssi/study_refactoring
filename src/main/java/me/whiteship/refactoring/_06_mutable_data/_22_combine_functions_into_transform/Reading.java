d07fc84b9d3871d29eb2710e5fd7dcca2905d415
2garam <2garamssi@gmail.com>
2022-07-08 15:51:53 +0900

    06-22 mutable_data : combine_functions_into_transform 여러 함수를 변환 함수로 묶기
    : 각 클래스에서 사용하고 있던 메소드를 레코르로 빼와서 공통적으로 사용하기.

--- src/main/java/me/whiteship/refactoring/_06_mutable_data/_22_combine_functions_into_transform/Reading.java
+++ src/main/java/me/whiteship/refactoring/_06_mutable_data/_22_combine_functions_into_transform/Reading.java
@@ -3,5 +3,7 @@ package me.whiteship.refactoring._06_mutable_data._22_combine_functions_into_tra
 import java.time.Month;
 import java.time.Year;
 
+
+// record 14버전부터 지원하는 불변 데이터 타입
 public record Reading(String customer, double quantity, Month month, Year year) {
 }
